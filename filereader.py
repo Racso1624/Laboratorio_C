ef808bd9fffe2d7343df54028137d3add400cb6d
Racso1624 <fer10_lb@outlook.com>
2023-03-30 19:43:17 -0600

    Se empieza limpieza

--- filereader.py
+++ filereader.py
@@ -13,5 +13,7 @@ class File(object):
                     if(line[i] == "(" and line[i + 1] == "*"):
                         comment_start = i
                     if(line[i] == "*" and line[i + 1] == ")"):
-                        comment_final = i
-                
+                        comment_final = i + 1
+                # GUARDAR LINEAS EN UNA NUEVA ESTRUCTURA DE DATOS PARA EL ARCHIVO
+                if(comment_start != None and comment_final != None):
+                    pass
