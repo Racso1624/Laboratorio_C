5ea81ef0936c57e1f141aeac7a909b7342d04d32
Racso1624 <fer10_lb@outlook.com>
2023-04-01 19:19:02 -0600

    Se obtienen los valores en let

--- filereader.py
+++ filereader.py
@@ -7,13 +7,8 @@ class File(object):
     def read(self):
         with open(self.filename, "r") as file:  
             for line in file:
-                comment_start = None
-                comment_final = None
-                for i in range(len(line)):
-                    if(line[i] == "(" and line[i + 1] == "*"):
-                        comment_start = i
-                    if(line[i] == "*" and line[i + 1] == ")"):
-                        comment_final = i + 1
-                # GUARDAR LINEAS EN UNA NUEVA ESTRUCTURA DE DATOS PARA EL ARCHIVO
-                if(comment_start != None and comment_final != None):
-                    pass
+                if('let' in line):
+                    line = line.replace('let ', '')
+                    line = line.replace(" ", "")
+                    line_separated = line.split('=')
+                    print(line_separated)
