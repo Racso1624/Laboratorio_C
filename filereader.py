03332c0e9aeb70bb10445baefb96538a5a14cc66
Racso1624 <fer10_lb@outlook.com>
2023-04-06 14:39:44 -0600

    Se sigue con los valores de let

--- filereader.py
+++ filereader.py
@@ -7,6 +7,7 @@ class File(object):
     def read(self):
         with open(self.filename, "r") as file:  
             for line in file:
+
                 if('let' in line):
                     line = line.replace('let ', '')
                     line = line.replace(" ", "")
@@ -17,6 +18,18 @@ class File(object):
                             # HACER LA CUENTA DE LOS -
                             # SI EXISTEN - SE REALIZA UNA ITERACION EN LOS CHARSET
                             # SI NO, SOLO SE TOMA EN CUENTA LOS VALORES QUE SE ENCUENTRAN DENTRO
-                            pass
+                            separator_counter = value.count('-')
+                            if(separator_counter != 0):
+                                index_counter = 1
+                                first_value = ""
+                                second_value = ""
+                                first_apostrophe = None
+                                second_apostrophe = None
+                                while(separator_counter != 0):
+                                    if(value[index_counter] == "'" and first_apostrophe == None):
+                                        first_apostrophe = index_counter
+                                    else:
+                                        second_apostrophe = index_counter
+                                
                         elif(value[1] == '"'):
                             pass
